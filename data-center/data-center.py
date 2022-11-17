48e09251afff8d69100c6eed40b54c48b36dba12
Ilnur Nasybullin <ilnur.nasybullin2000@gmail.com>
2022-11-17 12:14:13 +0300

    change worker

--- data-center/data-center.py
+++ data-center/data-center.py
@@ -6,36 +6,12 @@ from worker import Worker
 
 app = FastAPI()
 
-variable = None
-lock = threading.Lock()
-
-def read_variable():
-    global variable
-    return variable
-
-def clear_variable():
-    global variable
-    variable = None
-
-worker = Worker(lock=lock, supplier=read_variable, clear=clear_variable)
+worker = Worker()
 
 t = threading.Thread(target=worker.start)
 t.start()
 
 @app.get("/calculate")
 def calculate():
-    global lock, variable
-
-    lock.acquire()
-    task_is_accepted = False
-    try:
-        is_not_executed = read_variable() is None
-        if is_not_executed:
-            print("thread is not executed!")
-            variable = "execute!"
-            task_is_accepted = True
-    finally:
-        lock.release()
-
-
+    task_is_accepted = worker.try_submit_task(task="something task")
     return {"task_is_accepted": task_is_accepted}
--- data-center/worker.py
+++ data-center/worker.py
@@ -1,28 +1,46 @@
+import threading
 import time
+
 
 class Worker:
 
-    def __init__(self, lock, supplier, clear):
-        self._lock = lock
-        self._supplier = supplier
-        self._clear = clear
+    def __init__(self):
+        self._lock = threading.Lock()
+        self._task = None
+
+    def _read_task(self):
+        self._lock.acquire()
+        local_task = self._task
+        self._lock.release()
+        return local_task
+
+    def try_submit_task(self, task):
+        is_submitted = False
+
+        self._lock.acquire()
+        local_task = self._task
+        if local_task is None:
+            local_task = task
+            self._task = local_task
+            is_submitted = True
+        self._lock.release()
+        return is_submitted
+
+    def _clear(self):
+        self._lock.acquire()
+        self._task = None
+        self._lock.release()
 
     def start(self):
         while (True):
-            self._lock.acquire()
-            try:
-                variable = self._supplier()
-                if variable is not None:
-                    self._clear()
-                    print("global variable is cleaned!")
-            finally:
-                self._lock.release()
-
-            if variable is None:
-                time.sleep(5)
+            local_task = self._read_task()
+            if local_task is not None:
+                self._clear()
+                print("task is cleaned!")
+                self._do_something(local_task)
             else:
-                self._do_something(variable)
+                time.sleep(5)
 
-    def _do_something(self, variable):
+    def _do_something(self, task):
         print("execution!")
         time.sleep(15)
